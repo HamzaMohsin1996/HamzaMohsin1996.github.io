0d85dacebf4858d0e2561e184c9d61fcd44d0d7a
HamzaMohsin1996 <hamza95.hw@gmail.com>
2024-08-01 19:25:48 +0200

    new pages added html

--- js/custom.js
+++ js/custom.js
@@ -26,29 +26,6 @@ new WOW().init();
 
 // preloader
 
-var loader;
-
-function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.05);
-        }, 30);
-    }
-}
-
-function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
-});
-
 
 // laod more projects button
 
